4da08e0748b4a4c7fbfcb0aceb0450df2366e5a9
Mateusz <nowe7246@gmail.com>
2022-07-11 16:37:47 +0200

    Init commit.

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Header} from './components/layout/Header/Header';
+import {Menu} from './components/layout/Menu/Menu';
+import {Books} from "./components/Books/Books";
+
+import './App.css';
+
+
+
+export const App = () => {
+
+    return (
+        <div className="App">
+
+            {/*<Header/>*/}
+            {/*<Menu/>*/}
+            {/*<Books/>*/}
+
+        </div>
+    );
+}
+
+
