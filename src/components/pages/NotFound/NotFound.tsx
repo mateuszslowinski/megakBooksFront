b0ca6d10fa72c7221927154a271b51f7af39f119
Mateusz <nowe7246@gmail.com>
2022-09-12 16:41:17 +0200

    Added go to home page button.

--- src/components/pages/NotFound/NotFound.tsx
+++ src/components/pages/NotFound/NotFound.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
+import { Btn } from "../../common/Btn/Btn";
 
 import './NotFound.css';
 
@@ -7,6 +9,7 @@ export const NotFound = () => (
         <div className="error_message">
             <h1>Error 404.</h1>
             <p>Nie znaleziono podanej storny...</p>
+            <NavLink to="/"><Btn text="Strona główna"/></NavLink>
         </div>
     </div>
 );
