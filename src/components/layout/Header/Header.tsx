00f67d187fdd75b7176eeea1cdd41c9add2974a4
Mateusz <nowe7246@gmail.com>
2022-09-12 16:37:19 +0200

    Added getting user data in header depends on token.

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUserByToken } from "../../../features/user/userActions";
 import './Header.css'
 
 export const Header = () => {
+    const userToken = localStorage.getItem('userToken')
+        ? localStorage.getItem('userToken')
+        : null
+
+    const dispatch = useDispatch()
+    useEffect(() => {
+        if (userToken) {
+            // @ts-ignore
+            dispatch(fetchUserByToken())
+        }
+    }, [userToken, dispatch])
+
     return (
         <div className="site-header">
             <h1>Twój świat książek</h1>
