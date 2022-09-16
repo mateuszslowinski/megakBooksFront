59ee1f8737a4f4a0657c41c390e9d2b74a69de12
Mateusz <nowe7246@gmail.com>
2022-09-16 17:48:23 +0200

    Created get, add and remove books actions in user collection.

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,11 @@ import {configureStore} from '@reduxjs/toolkit';
 import {userSlice} from "../features/user/userSlice";
 
 
-export const store = configureStore({
-    reducer: {
-        users: userSlice.reducer
-    },
-});
 
+export const store:any = configureStore({
+    reducer:{
+        users:userSlice.reducer
+    }
+})
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
